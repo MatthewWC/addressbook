9cc5401deb297b95d67400b4ddf1e1838ed69378
Matthew Cook <matthew.w.cook94@gmail.com>
2020-03-01 11:05:23 -0500

    initial commit

---
+++ database.js
@@ -0,0 +1,36 @@
+//databsae module. it handles database operations
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+// must provide ssl true inorder to run
+const sequelize = new Sequelize(
+    process.env.DB_SCHEMA || 'postgres',
+    process.env.DB_USER || 'postgres',
+    process.env.DB_PASSWORD || '',
+    {
+        host: process.env.DB_HOST || 'locahost',
+        port: process.env.DB_PORT || 5432,
+        dialect: 'postgres',
+        ssl: true,
+        dialectOptions: {
+            ssl: process.env.DB_SSL == "true"
+        }
+    });
+
+const Person = sequelize.define('Person', {
+    firstName: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    lastName: {
+        type: Sequelize.STRING,
+        allowNull: true
+    },
+});
+
+module.exports = {
+    sequelize: sequelize,
+    Person: Person
+};
+
+
